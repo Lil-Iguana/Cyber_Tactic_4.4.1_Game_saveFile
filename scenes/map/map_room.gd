f0cf550818a75219cc031a9c1330475d44da852a
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-05-16 15:11:32 +0800

    Added Event Icon

--- scenes/map/map_room.gd
+++ scenes/map/map_room.gd
@@ -10,6 +10,7 @@ const ICONS := {
 	Room.Type.TREASURE: [preload("res://art/tile_0089.png"), Vector2.ONE],
 	Room.Type.CAMPFIRE: [preload("res://art/player_heart.png"), Vector2(0.6, 0.6)],
 	Room.Type.SHOP: [preload("res://art/gold.png"), Vector2(0.6, 0.6)],
+	Room.Type.EVENT: [preload("res://art/room_event_symbol.png"), Vector2.ONE],
 	Room.Type.BOSS: [preload("res://art/MalwareBoss.png"), Vector2(1.25, 1.25)],
 }
 
