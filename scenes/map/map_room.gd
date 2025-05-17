92cd19415476e6beda426786a9e40cca871de92f
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-05-17 18:42:51 +0800

    Update Enemy Icon

--- scenes/map/map_room.gd
+++ scenes/map/map_room.gd
@@ -6,7 +6,7 @@ signal selected(room: Room)
 
 const ICONS := {
 	Room.Type.NOT_ASSIGNED: [null, Vector2.ONE],
-	Room.Type.MONSTER: [preload("res://art/Malware.png"), Vector2.ONE],
+	Room.Type.MONSTER: [preload("res://art/Enemy_Icon.png"), Vector2.ONE],
 	Room.Type.TREASURE: [preload("res://art/tile_0089.png"), Vector2.ONE],
 	Room.Type.CAMPFIRE: [preload("res://art/player_heart.png"), Vector2(0.6, 0.6)],
 	Room.Type.SHOP: [preload("res://art/gold.png"), Vector2(0.6, 0.6)],
