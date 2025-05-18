46c8ae746b6aab6c1d84c4fa729942ddd90f9893
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-05-18 21:31:31 +0800

    Added event room script & folder

---
+++ scenes/event_rooms/event_room.gd
@@ -0,0 +1,9 @@
+class_name EventRoom
+extends Control
+
+@export var character_stats: CharacterStats
+@export var run_stats: RunStats
+@export var thread_handler: ThreadHandler
+
+func setup() -> void:
+	pass
