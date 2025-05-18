0204c03842557cca56d043b1015e9d1f6558a709
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-05-18 15:03:46 +0800

    Update warrior_cybersword_attack.gd
    Added elemental

--- characters/warrior/cards/warrior_cybersword_attack.gd
+++ characters/warrior/cards/warrior_cybersword_attack.gd
@@ -1,7 +1,9 @@
 extends Card
 
 
-var base_damage := 60
+var base_damage := 6
+
+@export var element := Stats.Element.FIRE
 
 
 func get_default_tooltip() -> String:
@@ -9,16 +11,23 @@ func get_default_tooltip() -> String:
 
 
 func get_updated_tooltip(player_modifiers: ModifierHandler, enemy_modifiers: ModifierHandler) -> String:
-	var modified_dmg := player_modifiers.get_modified_value(base_damage, Modifier.Type.DMG_DEALT)
+	var dmg := player_modifiers.get_modified_value(base_damage, Modifier.Type.DMG_DEALT)
 
+	if enemy_modifiers and enemy_modifiers.has_modifier(Modifier.Type.DMG_TAKEN):
+		var enemy_stats = enemy_modifiers.get_parent().stats
+		var elem_mult = enemy_stats.get_elemental_multiplier(element)
+		dmg = floor(dmg * elem_mult)
+	
 	if enemy_modifiers:
-		modified_dmg = enemy_modifiers.get_modified_value(modified_dmg, Modifier.Type.DMG_TAKEN)
+		dmg = enemy_modifiers.get_modified_value(dmg, Modifier.Type.DMG_TAKEN)
 		
-	return tooltip_text % modified_dmg
+	return tooltip_text % dmg
 
 
 func apply_effects(targets: Array[Node], modifiers: ModifierHandler) -> void:
+	var raw = modifiers.get_modified_value(base_damage, Modifier.Type.DMG_DEALT)
 	var damage_effect := DamageEffect.new()
-	damage_effect.amount = modifiers.get_modified_value(base_damage, Modifier.Type.DMG_DEALT)
+	damage_effect.amount = raw
+	damage_effect.element = element
 	damage_effect.sound = sound
 	damage_effect.execute(targets)
