cef318634cebe85ae47869fa8f275e566d89471e
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-05-18 21:28:13 +0800

    Update run.gd
    Added event room functions

--- scenes/run/run.gd
+++ scenes/run/run.gd
@@ -126,6 +126,7 @@ func _setup_event_connections() -> void:
 	Events.map_exited.connect(_on_map_exited)
 	Events.shop_exited.connect(_show_map)
 	Events.treasure_room_exited.connect(_on_treasure_room_exited)
+	Events.event_room_exited.connect(_show_map)
 	Events.bestiary_exited.connect(_show_map)
 	
 	battle_button.pressed.connect(_change_view.bind(BATTLE_SCENE))
@@ -196,6 +197,14 @@ func _on_shop_entered() -> void:
 	shop.populate_shop()
 
 
+func _on_event_room_entered(room: Room) -> void:
+	var event_room := _change_view(room.event_scene) as EventRoom
+	event_room.character_stats = character
+	event_room.run_stats = stats
+	event_room.thread_handler = thread_handler
+	event_room.setup()
+
+
 func _on_battle_won() -> void:
 	if map.floors_climbed == MapGenerator.FLOORS:
 		var win_screen := _change_view(WIN_SCREEN_SCENE) as WinScreen
@@ -217,5 +226,7 @@ func _on_map_exited(room: Room) -> void:
 			_on_campfire_entered()
 		Room.Type.SHOP:
 			_on_shop_entered()
+		Room.Type.EVENT:
+			_on_event_room_entered(room)
 		Room.Type.BOSS:
 			_on_battle_room_entered(room)
