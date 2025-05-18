4ac468a1c3f68eb439cbd13caef80a10e13269c3
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-05-18 14:36:10 +0800

    Update damage_effect.gd
    Added element type

--- effect/damage_effect.gd
+++ effect/damage_effect.gd
@@ -3,6 +3,7 @@ extends Effect
 
 var amount := 0
 var receiver_modifier_type := Modifier.Type.DMG_TAKEN
+@export var element := Stats.Element.PHYSICAL
 
 
 func execute(targets: Array[Node]) -> void:
@@ -10,5 +11,7 @@ func execute(targets: Array[Node]) -> void:
 		if not target:
 			continue
 		if target is Enemy or target is Player:
-			target.take_damage(amount, receiver_modifier_type)
+			var elem_mult = target.stats.get_elemental_multiplier(element)
+			var raw_damage = floor(amount * elem_mult)
+			target.take_damage(raw_damage, receiver_modifier_type)
 			SFXPlayer.play(sound)
