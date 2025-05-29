35e433131e72b2178c65e34c1e8384ba8462c145
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-05-29 13:53:45 +0800

    Update toxic_muscle_buff_action.gd
    Fix a bug

--- enemies_stats/toxic_ghost/toxic_muscle_buff_action.gd
+++ enemies_stats/toxic_ghost/toxic_muscle_buff_action.gd
@@ -4,7 +4,7 @@ const POWER_STATUS = preload("res://statuses/power_up.tres")
 
 @export var stacks_per_action := 2
 
-var hp_threshold := 25
+var hp_threshold := 10
 var usages := 0
 
 
@@ -12,7 +12,6 @@ func is_performable() -> bool:
 	var hp_under_threshold := enemy.stats.health <= hp_threshold
 	
 	if usages == 0 or (usages == 1 and hp_under_threshold):
-		usages += 1
 		return true
 	
 	return false
@@ -22,6 +21,7 @@ func perform_action() -> void:
 	if not enemy or not target:
 		return
 	
+	usages += 1
 	var status_effect := StatusEffect.new()
 	var power := POWER_STATUS.duplicate()
 	power.stacks = stacks_per_action
