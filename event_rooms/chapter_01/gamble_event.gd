7e68ff0eb4df368d3f67666217feef5722b09788
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-05-30 10:18:17 +0800

    Changes

--- event_rooms/chapter_01/gamble_event.gd
+++ event_rooms/chapter_01/gamble_event.gd
@@ -7,11 +7,9 @@ extends EventRoom
 @onready var event_aftermath: VBoxContainer = %EventAfterMath
 @onready var aftermath_text: RichTextLabel = %AftermathText
 
-var text: String = "[center]\"Playing safe? Never thought you were the type.\"[/center]"
+var text: String = "[center]\"Wrong! The correct answer is B. The Melissa virus caused USD 80 Million of damages.\"[/center]"
 
 func setup() -> void:
-	fifty_button.disabled = run_stats.gold < 50
-	thirty_button.disabled = run_stats.gold < 50
 
 	fifty_button.event_button_callback = bet_50
 	thirty_button.event_button_callback = bet_30
@@ -20,22 +18,14 @@ func setup() -> void:
 
 func bet_30() -> void:
 	thirty_button.disabled = true
-	run_stats.gold -= 50
-	text = "[center]\"Too bad...\"[/center]"
-	if RNG.instance.randf() < 0.3:
-		run_stats.gold += 200
-		text = "[center]\"Nice going!\"[/center]"
 	
 	conclude_event()
 
 
 func bet_50() -> void:
 	fifty_button.disabled = true
-	run_stats.gold -= 50
-	text = "[center]\"Too bad...\"[/center]"
-	if RNG.instance.randf() < 0.5:
-		run_stats.gold += 100
-		text = "[center]\"Nice going!\"[/center]"
+	text = "[center]\"Correct! Here is your award!\"[/center]"
+	run_stats.gold += 200
 	
 	conclude_event()
 
