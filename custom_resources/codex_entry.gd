f19b1bc53748d6dabf86874982e8036a10c190d3
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-07-19 20:49:52 +0800

    Update codex

--- custom_resources/codex_entry.gd
+++ custom_resources/codex_entry.gd
@@ -4,6 +4,6 @@ extends Resource
 @export var id: String
 @export var name: String
 @export var art: Texture
-@export var tooltip_text: String
-@export var tooltip_text_real: String
+@export_multiline var tooltip_text: String
+@export_multiline var tooltip_text_real: String
 @export var is_unlocked: bool = false
