876b42843f7435e433144fa652648d0abaa27035
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-08-11 21:45:16 +0800

    Fixed a visual bug for choosing cards

--- scenes/ui/card_hand_choice_ui.gd
+++ scenes/ui/card_hand_choice_ui.gd
@@ -10,23 +10,29 @@ const HOVER_STYLEBOX := preload("res://scenes/card_ui/card_hover_stylebox.tres")
 
 @onready var visuals: CardVisuals = $Visuals
 
+# Helper to duplicate + tint a StyleBoxFlat according to card.type
+func apply_tinted_style(template: StyleBoxFlat) -> void:
+	if card == null:
+		return
+	var sb = template.duplicate() as StyleBoxFlat
+	sb.bg_color = Card.type_color(card.type)
+	visuals.panel.set("theme_override_styles/panel", sb)
 
 func _on_visuals_gui_input(event: InputEvent) -> void:
-	if event.is_action_pressed("left_mouse"):
+	if event.is_action_pressed("left_mouse") and card != null:
 		hand_choice_tooltip_requested.emit(card)
 
-
 func _on_visuals_mouse_entered() -> void:
-	visuals.panel.set("theme_override_styles/panel", HOVER_STYLEBOX)
-
+	apply_tinted_style(HOVER_STYLEBOX)
 
 func _on_visuals_mouse_exited() -> void:
-	visuals.panel.set("theme_override_styles/panel", BASE_STYLEBOX)
-
+	apply_tinted_style(BASE_STYLEBOX)
 
 func set_card(value: Card) -> void:
 	if not is_node_ready():
 		await ready
-	
+
 	card = value
 	visuals.card = card
+	# apply initial tinted base once the card is assigned
+	apply_tinted_style(BASE_STYLEBOX)
