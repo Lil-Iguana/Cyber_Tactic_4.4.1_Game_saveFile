fa65fb64b48132e1db0616c6b522fc0bc8cd4c80
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-09-08 18:42:23 +0800

    Update run_stats.gd
    Added chapter variable for future use

--- custom_resources/run_stats.gd
+++ custom_resources/run_stats.gd
@@ -14,6 +14,7 @@ const BASE_RARE_WEIGHT := 0.3
 @export_range(0.0, 10.0) var common_weight := BASE_COMMON_WEIGHT
 @export_range(0.0, 10.0) var uncommon_weight := BASE_UNCOMMON_WEIGHT
 @export_range(0.0, 10.0) var rare_weight := BASE_RARE_WEIGHT
+@export_range(0,1) var chapter := 0
 
 
 func set_gold(new_amount: int) -> void:
