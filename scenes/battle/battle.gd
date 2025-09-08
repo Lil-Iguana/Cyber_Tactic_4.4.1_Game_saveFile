5adee4565880f2ca982c4e86c4ab28e74cc94cc0
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-09-08 20:46:02 +0800

    Update battle.gd
    Music

--- scenes/battle/battle.gd
+++ scenes/battle/battle.gd
@@ -4,6 +4,7 @@ extends Node2D
 @export var battle_stats: BattleStats
 @export var char_stats: CharacterStats
 @export var music: AudioStream
+@export var boss_music: AudioStream
 @export var threads: ThreadHandler
 
 @onready var battle_ui: BattleUI = $BattleUI
@@ -12,6 +13,7 @@ extends Node2D
 @onready var player: Player = $Player
 @onready var hand: HBoxContainer = %Hand
 
+@onready var map: Map = get_node_or_null("../../Map")
 
 
 func _ready() -> void:
@@ -21,11 +23,16 @@ func _ready() -> void:
 	Events.player_press_end_turn_button.connect(player_handler.end_turn)
 	Events.player_hand_discarded.connect(enemy_handler.start_turn)
 	Events.player_died.connect(_on_player_died)
+	
+	Events.music_set.connect(_on_music_set)
 
 
 func start_battle() -> void:
 	get_tree().paused = false
-	MusicPlayer.play(music, true)
+	
+	if battle_stats.battle_tier == 2:
+		boss_music = map.boss_music
+		MusicPlayer.play_music(boss_music, true)
 	
 	battle_ui.char_stats = char_stats
 	player.stats = char_stats
@@ -63,3 +70,18 @@ func _on_threads_activated(type: ThreadPassive.Type) -> void:
 			battle_ui.initialize_card_pile_ui()
 		ThreadPassive.Type.END_OF_COMBAT:
 			Events.battle_over_screen_requested.emit("Victorious!", BattleOverPanel.Type.WIN)
+
+
+func _on_music_set() -> void:
+	music = map.music
+	boss_music = map.boss_music
+	#print("music copied from map")
+	
+	if battle_stats.battle_tier == 2:
+		#print("playing boss music")
+		MusicPlayer.play_music(boss_music, true)
+	elif not MusicPlayer.already_playing_music():
+		MusicPlayer.play_music(music, true)
+	else:
+		pass
+		#print("music already playing")
