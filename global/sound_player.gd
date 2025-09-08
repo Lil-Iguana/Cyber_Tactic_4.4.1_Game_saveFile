43cd7fc8590294d00e53b3928e824ced1be5c85f
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-09-08 18:33:27 +0800

    Update sound_player.gd

--- global/sound_player.gd
+++ global/sound_player.gd
@@ -17,7 +17,34 @@ func play(audio: AudioStream, single=false) -> void:
 			break
 
 
+#Same function as the previous one, but without the pitch change for SFX.
+func play_music(audio: AudioStream, single = false) -> void:
+	if not audio:
+		return
+	
+	if single:
+		stop()
+	
+	for player in get_children():
+		player = player as AudioStreamPlayer
+		if not player.playing:
+			player.stream = audio
+			player.play()
+			break
+
+
 func stop() -> void:
 	for player in get_children():
 		player = player as AudioStreamPlayer
 		player.stop()
+
+
+func already_playing_music() -> bool:
+	for player in get_children():
+		player = player as AudioStreamPlayer
+		if player.playing:
+			print("already playing music")
+			return true
+			
+	print ("no music")
+	return false
