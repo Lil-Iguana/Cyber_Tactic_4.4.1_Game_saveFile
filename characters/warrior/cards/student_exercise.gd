45e0413790e62d1b412ccfd90980501fc738e962
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-09-22 14:33:43 +0800

    New card

---
+++ characters/warrior/cards/student_exercise.gd
@@ -0,0 +1,13 @@
+extends Card
+
+const POWER_UP_STATUS := preload("res://statuses/powering_up.tres")
+
+var power_stacks := 2
+
+func apply_effects(targets: Array[Node], _modifiers: ModifierHandler):
+	SFXPlayer.play(sound)
+	var status_effect := StatusEffect.new()
+	var power_up := POWER_UP_STATUS.duplicate()
+	power_up.stacks = power_stacks
+	status_effect.status = power_up
+	status_effect.execute(targets)
