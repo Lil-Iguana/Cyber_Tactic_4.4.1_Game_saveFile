4fce5dde09546ae4d2846599cee1fdd1725846cb
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-09-24 20:32:15 +0800

    Added new thread

---
+++ threads/firewall.gd
@@ -0,0 +1,23 @@
+extends ThreadPassive
+
+@export var block := 5
+
+var thread_ui: ThreadUI
+
+func initialize_relic(owner: ThreadUI) -> void:
+	thread_ui = owner
+	Events.player_press_end_turn_button.connect(_on_player_press_end_turn_button)
+
+
+func deactivate_relic(_owner: ThreadUI) -> void:
+	Events.player_press_end_turn_button.disconnect(_on_player_press_end_turn_button)
+
+func _on_player_press_end_turn_button() -> void:
+	var player := thread_ui.get_tree().get_first_node_in_group("player")
+	if player.stats.block == 0:
+		var block_effect := BlockEffect.new()
+		block_effect.amount = block
+		block_effect.receiver_modifier_type = Modifier.Type.NO_MODIFIER
+		block_effect.execute([player])
+		
+		thread_ui.flash()
