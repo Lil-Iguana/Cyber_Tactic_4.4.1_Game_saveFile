1e52bc840ec3df482cb27b7a96b150d261169df5
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-09-25 11:42:26 +0800

    Renamed

--- threads/confusing_staff.gd
+++ threads/confusing_staff.gd
@@ -17,5 +17,5 @@ func _on_player_hand_drawn() -> void:
 	var player_handler := thread_ui.get_tree().get_first_node_in_group("player_handler") as PlayerHandler
 	
 	for card_ui: CardUI in player_handler.hand.get_children():
-		card_ui.card.cost = RNG.instance.randi_range(0, 3)
+		card_ui.card.cost = RNG.instance.randi_range(0, 1)
 		card_ui.card = card_ui.card
