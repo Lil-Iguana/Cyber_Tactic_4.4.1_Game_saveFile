2d1130e6b49e1febd8ecb063bbf91c96debcf96c
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-09-25 11:11:38 +0800

    Created new card

---
+++ characters/warrior/cards/scanwave.gd
@@ -0,0 +1,30 @@
+extends Card
+
+const VULNERABLE_STATUS := preload("res://statuses/vulnerable.tres")
+
+var base_damage := 4
+var exposed_duration := 1
+
+func apply_effects(targets: Array[Node], modifiers: ModifierHandler):
+	var damage_effect := DamageEffect.new()
+	damage_effect.amount = modifiers.get_modified_value(base_damage, Modifier.Type.DMG_DEALT)
+	damage_effect.sound = sound
+	damage_effect.execute(targets)
+	
+	@warning_ignore("shadowed_variable_base_class")
+	for target in targets:
+		var status_effect := StatusEffect.new()
+		var vulnerable := VULNERABLE_STATUS.duplicate()
+		vulnerable.duration = exposed_duration
+		status_effect.status = vulnerable
+		status_effect.execute([target])
+
+func get_default_tooltip() -> String:
+	return tooltip_text % base_damage
+
+func get_updated_tooltip(player_modifiers: ModifierHandler, enemy_modifiers: ModifierHandler) -> String:
+	var modified_damage := player_modifiers.get_modified_value(base_damage, Modifier.Type.DMG_DEALT)
+	if enemy_modifiers:
+		modified_damage = enemy_modifiers.get_modified_value(modified_damage, Modifier.Type.DMG_TAKEN)
+	
+	return tooltip_text % modified_damage
