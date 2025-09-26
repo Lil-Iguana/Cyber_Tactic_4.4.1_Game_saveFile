c13e07b3ca9577a461a12a902639707f8e78a995
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-09-27 07:02:18 +0800

    Added skip

--- video.gd
+++ video.gd
@@ -5,3 +5,8 @@ const RUN_SCENE := preload("res://scenes/run/run.tscn")
 func _on_video_stream_player_finished() -> void:
 	get_tree().change_scene_to_packed(RUN_SCENE)
 	queue_free()
+
+
+func _on_skip_button_pressed() -> void:
+	get_tree().change_scene_to_packed(RUN_SCENE)
+	queue_free()
