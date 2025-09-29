fdde688813a5ea8b2cc64636593af5fe06ca4af0
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-09-29 21:07:00 +0800

    Update strong_password.gd
    Fix bug

--- characters/warrior/cards/strong_password.gd
+++ characters/warrior/cards/strong_password.gd
@@ -1,16 +1,11 @@
 extends Card
 
-@export var optional_sound: AudioStream
+var base_damage := 0
 
-
-func get_default_tooltip() -> String:
-	return tooltip_text
-
-
-func get_updated_tooltip(_player_modifiers: ModifierHandler, _enemy_modifiers: ModifierHandler) -> String:
-	return tooltip_text
-
-
-func apply_effects(targets: Array[Node], _modifiers: ModifierHandler) -> void:
-	print("My awesome card has been played!")
-	print("Targets: %s" % targets)
+func apply_effects(targets: Array[Node], modifiers: ModifierHandler) -> void:
+	var player := targets[0].get_tree().get_nodes_in_group("player")[0] as Player
+	base_damage = player.stats.block
+	var damage_effect := DamageEffect.new()
+	damage_effect.amount = modifiers.get_modified_value(base_damage, Modifier.Type.DMG_DEALT)
+	damage_effect.sound = sound
+	damage_effect.execute(targets)
