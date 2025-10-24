5f3da4f1cc8ee4de2cff194e01da6dc4b39425e7
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-10-24 20:55:17 +0800

    Update First Battle

---
+++ scenes/ui/battle_over_panel_sim.gd
@@ -0,0 +1,21 @@
+class_name BattleOverPanelSim
+extends Panel
+
+const RUN = "res://scenes/run/run.tscn"
+
+enum Type {WIN, LOSE}
+
+@onready var label: Label = %Label
+@onready var continue_button: Button = %ContinueButton
+
+
+func _ready() -> void:
+	continue_button.pressed.connect(get_tree().change_scene_to_file.bind(RUN))
+	Events.battle_over_screen_requested.connect(show_screen)
+
+
+func show_screen(text: String, type: Type) -> void:
+	label.text = text
+	continue_button.visible = type == Type.WIN
+	show()
+	get_tree().paused = true
