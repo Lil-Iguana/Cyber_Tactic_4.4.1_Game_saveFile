8fbdab4dd300c188001322aa9faecaac1b378653
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-10-25 21:37:31 +0800

    Update battle_ui_sim.gd

--- scenes/battle/battle_ui_sim.gd
+++ scenes/battle/battle_ui_sim.gd
@@ -12,16 +12,13 @@ func _ready() -> void:
 	Events.player_hand_drawn.connect(_on_player_hand_drawn)
 	end_turn_button.pressed.connect(_on_end_turn_button_pressed)
 
-
 func _set_char_stats(value: CharacterStats) -> void:
 	char_stats = value
 	mana_ui.char_stats = char_stats
 	hand.char_stats = char_stats
 
-
 func _on_player_hand_drawn() -> void:
 	end_turn_button.disabled = false
-
 
 func _on_end_turn_button_pressed() -> void:
 	end_turn_button.disabled = true
