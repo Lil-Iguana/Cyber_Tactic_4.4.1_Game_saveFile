6c8b73df9877730ac7caf26f0ba648ba68f5c399
Lil Iguana <opiana.218936@novaliches.sti.edu.ph>
2025-11-02 17:53:53 +0800

    Update return_top_deck_effect.gd

--- effect/return_top_deck_effect.gd
+++ effect/return_top_deck_effect.gd
@@ -2,19 +2,19 @@ class_name ReturnTopDeckEffect
 extends Effect
 
 func execute(targets: Array[Node]) -> void:
-    if targets.is_empty():
-        print("no target")
-        return
+	if targets.is_empty():
+		print("no target")
+		return
 
-    var player_handler := targets[0].get_tree().get_first_node_in_group("player_handler") as PlayerHandler
+	var player_handler := targets[0].get_tree().get_first_node_in_group("player_handler") as PlayerHandler
 
-    if not player_handler:
-        print("no player handler")
-        return
+	if not player_handler:
+		print("no player handler")
+		return
 
-    # connect the card_chosen signal if it's not already, and request the view
-    if not Events.card_chosen.is_connected(player_handler.return_to_top_deck):
-        Events.card_chosen.connect(player_handler.return_to_top_deck, CONNECT_ONE_SHOT)
-        
-    Events.hand_choice_requested.emit("Choose a card")
-    
+	# connect the card_chosen signal if it's not already, and request the view
+	if not Events.card_chosen.is_connected(player_handler.return_to_top_deck):
+		Events.card_chosen.connect(player_handler.return_to_top_deck, CONNECT_ONE_SHOT)
+		
+	Events.hand_choice_requested.emit("Choose a card")
+	
